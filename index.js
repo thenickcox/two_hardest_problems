4f1da884d3d9db19a1f094af61068ae65732ac4d
Nick Cox <nick@nickcox.me>
2016-03-28 21:03:42 -0700

    refactor into es2015, use watson API

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('/Users/nickcox/projects/two_hardest_problems/node_modules/dotenv')
   .config({ path: '/Users/nickcox/projects/two_hardest_problems/.env' });
 
@@ -17,104 +19,99 @@ var T = new Twit({
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 });
 
-var chooseRandom = function (arr) {
-  var index = Math.floor(Math.random() * arr.length);
-  return arr[index];
-}
-
-var topStorySections = [
-  'world',
-  'national',
-  'politics',
-  'business',
-  'opinion',
-  'technology',
-  'science',
-  'health',
-  'sports',
-  'arts',
-  'fashion',
-]
-
-
-var getNounPhrases = function(headline){
-  var deferred = Q.defer();
-  unirest.post("https://textanalysis.p.mashape.com/textblob-noun-phrase-extraction")
-    .header("X-Mashape-Key", process.env.MASHAPE_API_KEY)
-    .header("Content-Type", "application/x-www-form-urlencoded")
-    .header("Accept", "application/json")
-    .send("text=" + headline)
-    .end(function (result) {
-       var phrases = result.body.noun_phrases,
-           firstProblem = chooseRandom(phrases),
-           secondProblem = chooseRandom(phrases);
-      while (firstProblem === secondProblem){
-        firstProblem = chooseRandom(phrases);
-        secondProblem = chooseRandom(phrases);
-      }
-      var problems = [firstProblem, secondProblem];
-      deferred.resolve(problems);
-    });
-  return deferred.promise;
-}
-
-
-var leads = ['two hardest', 'only two hard'];
-var subjects = ['computer science', 'programming', 'computers'];
-
-var composeTweet = function(problems){
-  var deferred = Q.defer();
-
-  var tweetBody = function (problems) {
-    var lead = leads[Math.floor(Math.random() * leads.length)];
-    var subject = subjects[Math.floor(Math.random() * subjects.length)];
-    return "The " + lead + " problems in " + subject + " are " + problems[0] + " and " + problems[1] + ".";
+class Bot {
+  chooseRandom(arr) {
+    var index = Math.floor(Math.random() * arr.length);
+    return arr[index];
   }
+  getNounPhrases(headline) {
+    var _this = this;
+    var deferred = Q.defer();
+    unirest.post("https://textanalysis.p.mashape.com/textblob-noun-phrase-extraction")
+      .header("X-Mashape-Key", process.env.MASHAPE_API_KEY)
+      .header("Content-Type", "application/x-www-form-urlencoded")
+      .header("Accept", "application/json")
+      .send("text=" + headline)
+      .end(function (result) {
+         var phrases = result.body.noun_phrases,
+             firstProblem = _this.chooseRandom(phrases),
+             secondProblem = _this.chooseRandom(phrases);
+        while (firstProblem === secondProblem){
+          firstProblem = _this.chooseRandom(phrases);
+          secondProblem = _this.chooseRandom(phrases);
+        }
+        var problems = [firstProblem, secondProblem];
+        deferred.resolve(problems);
+      });
+    return deferred.promise;
+  }
+  leads(){
+    return ['two hardest', 'only two hard'];
+  }
+  subjects() {
+    return ['computer science', 'programming', 'computers'];
+  }
+  composeTweet(problems) {
+    var deferred = Q.defer();
 
-  T.post('statuses/update', { status: tweetBody(problems) }, function(err, data, response) {
-    if (err) {
-      deferred.reject(err);
+    var _this = this;
+    var tweetBody = function (problems) {
+      var lead = _this.leads()[Math.floor(Math.random() * _this.leads.length)];
+      var subject = _this.subjects()[Math.floor(Math.random() * _this.subjects().length)];
+      deferred.resolve(`The ${lead} problems in ${subject} are ${problems[0]} and ${problems[1]}.`);
     }
-    console.log(data);
-    deferred.resolve(data);
-  });
-  return deferred.promise;
-}
-
-var getNYTimesHeadline = function(){
-  var deferred = Q.defer(),
-      nyTimesUrl = "http://api.nytimes.com/svc/topstories/v1/" +
-                   chooseRandom(topStorySections) +
-                   ".json?api-key=" +
-                   process.env.NY_TIMES_API_KEY;
-  http.get(nyTimesUrl, function(res){
-    res.setEncoding('utf-8');
-    var body = '';
-    res.on('data', function(chunk) {
-      body += chunk;
-    });
-    res.on('end', function() {
-      var newBody = JSON.parse(body);
-          results = newBody.results;
-          resLength = results.length;
-      for (var i = 0; i < resLength; i++) {
-        headline = results[i].title;
-        headlines.push(headline);
+    return deferred.promise;
+  }
+  postTweet(tweet) {
+    console.log("Posting tweet: " + tweet);
+    T.post('statuses/update', { status: tweet }, function(err, data, response) {
+      if (err) {
+        console.log(err);
       }
-      headlineToUse = chooseRandom(headlines);
-      deferred.resolve(headlineToUse);
+      console.log(data);
     });
-  }).on('error', function(e){
-    deferred.reject(e);
-  });
-  return deferred.promise;
+  }
+  getHeadline() {
+    var deferred = Q.defer(),
+        _this = this,
+        apiUrl = `http://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=${process.env.WATSON_API_KEY}&outputMode=json&start=now-1d&end=now&count=10&&return=enriched.url.title`;
+    http.get(apiUrl, function(res){
+      res.setEncoding('utf-8');
+      var body = '';
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+        console.log(body);
+        var newBody = JSON.parse(body);
+        var results = newBody.result.docs;
+        var resLength = results.length;
+        for (var i = 0; i < resLength; i++) {
+          var headline = results[i].source.enriched.url.title;
+          headlines.push(headline);
+        }
+        headlineToUse = _this.chooseRandom(headlines);
+        deferred.resolve(headlineToUse);
+      });
+    }).on('error', function(e){
+      deferred.reject(e);
+    });
+    return deferred.promise;
+  }
 }
 
-getNYTimesHeadline()
-  .then(function(headline){
-    return getNounPhrases(headline)
-  })
-  .then(function(nounPhrases){
-    return composeTweet(nounPhrases)
-  })
-  .done();
+var run = ( () => {
+  const bot = new Bot();
+
+  bot.getHeadline()
+     .then(function(headline){
+       return bot.getNounPhrases(headline)
+      })
+      .then(function(nounPhrases){
+        return bot.composeTweet(nounPhrases)
+      })
+      .then(function(tweet){
+        return bot.postTweet(tweet);
+      })
+      .done();
+})();
